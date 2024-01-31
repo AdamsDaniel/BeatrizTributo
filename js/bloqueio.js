3157c724a6778e4637e01e3c312cfec45423df34
Daniel Knebel Adams <dnkndanieltrab@gmail.com>
2024-01-30 23:11:36 -0300

    subindo arquivos

---
+++ js/bloqueio.js
@@ -0,0 +1,4 @@
+if (window.location.href.indexOf('github.io') > -1) {
+    // Redirecionar para a página inicial ou outra página desejada
+    window.location.href = 'https://adamsdaniel.github.io/BeatrizTributo';
+}
